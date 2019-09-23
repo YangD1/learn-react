1e6a5a924026bb9f8b3643063e46698a1407af1b
YonDee <im.yondee@gmail.com>
2019-09-23 16:47:22 +0800

    [my-app]井字棋DEMO: 轮流落子，修改LEARN_NOTE错误

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // 受控组件
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props){
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -21,26 +19,30 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares:Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
     return (
-      <Square 
-        value={this.state.squares[i]} 
+      <Square
+        value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
       />
     );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
